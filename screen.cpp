f6ed7d79344ee72ad0fcdef3d8d23426a5aa76b2
HemersonRafael <rafaelpontes1995@gmail.com>
2017-10-06 08:06:08 -0300

    ...

--- screen.cpp
+++ screen.cpp
@@ -7,16 +7,18 @@ using namespace std;
 Screen::Screen(int _nlin, int _ncol){
     nlin = _nlin;
     ncol = _ncol;
-    m.resize();
-        for(int i = 0 ; i < m ; ++i)
-        {
-            //Grow Columns by n
-            a[i].resize(n);
-        }
+    mat.resize(_nlin);
+
+    for(int i=0; i<mat.size(); ++i){
+        mat[i].resize(_ncol);
+    }
 }
 
 void Screen::setPixel(int _x, int _y){
     mat[_x][_y] = brush;
+}
+
+void Screen::clear(){
 
 }
 
@@ -24,6 +26,6 @@ void Screen::setBrush(char _brush){
     brush = _brush;
 }
 
-ostream &Screen::operator<<(ostream &os, Screen &t){
+ostream &operator<<(ostream &os, Screen &t){
 
 }
--- screen.pro
+++ screen.pro
@@ -4,8 +4,16 @@ CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += main.cpp \
-    screen.cpp
+    screen.cpp \
+    figurageometrica.cpp \
+    reta.cpp \
+    circulo.cpp \
+    retangulo.cpp
 
 HEADERS += \
-    screen.h
+    screen.h \
+    figurageometrica.h \
+    reta.h \
+    circulo.h \
+    retangulo.h
 
